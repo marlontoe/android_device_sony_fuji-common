365b46a23a80a5dd9c41c2e789cc80d7f417abbf
Warren <deed821@gmail.com>
2014-07-17 17:14:04 +1000

    use sony GPS config
    Change-Id: Ia5972636754fc9580b0f2fb21f599fc97d795172
    
    Conflicts:
    	fuji.mk

--- fuji.mk
+++ fuji.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
 DEVICE_PACKAGE_OVERLAYS += device/sony/fuji-common/overlay
 
 # Permissions
@@ -139,6 +136,10 @@ PRODUCT_PACKAGES += \
 # Bluetooth vendor config
 PRODUCT_COPY_FILES += \
     device/sony/fuji-common/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# GPS Config
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/system/etc/gps.conf:system/etc/gps.conf
 
 # Power HAL
 PRODUCT_PACKAGES += \
