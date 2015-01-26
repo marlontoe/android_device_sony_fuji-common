9134a64dd631fad016f29a1189ff71d8d618e511
marlontoe <inclusionrec@googlemail.com>
2015-01-26 16:07:06 +0100

    readded some configs/layers from opensemc repo

--- fuji.mk
+++ fuji.mk
@@ -266,6 +266,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.sf.hw=1 \
+    persist.hwc.mdpcomp.enable=true \
+    debug.composition.type=dyn \
+    debug.mdpcomp.maxlayer=3 \
     debug.mdpcomp.logs=0 \
     debug.egl.recordable.rgba8888=1
 
@@ -275,6 +278,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # More display props - double check these!
 PRODUCT_PROPERTY_OVERRIDES += \
+    dev.pm.dyn_samplingrate=1 \
     debug.hwc.dynThreshold=1.9
 
 # Low Power Audio Decoding
